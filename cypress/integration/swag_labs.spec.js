ba077ca759e2004836872c6f86c1bfd57b5e902a
Bruno Santos <brunogsantoss@outlook.com>
2024-04-23 21:41:10 -0300

    More one scenario

--- cypress/integration/swag_labs.spec.js
+++ cypress/integration/swag_labs.spec.js
@@ -2,7 +2,7 @@
 
 describe('My First Test', () => {
     beforeEach(function(){
-        cy.visit('https://www.saucedemo.com')
+        cy.page()
     })
 
     it('Página deve conter o Título', () => {
@@ -10,50 +10,34 @@ describe('My First Test', () => {
     })
 
     it('Logar com um usuário standard_user', () => {
-        cy.get('[data-test="username"]').type('standard_user')
-        cy.get('[data-test="password"]').type('secret_sauce')
-        cy.get('[data-test="login-button"]').click()
-        cy.get('[data-test="title"]').should('be.visible')
+        cy.login_standardUser();
     })
 
     it('Logar com um usuário locked_out_user', () => {
-        cy.get('[data-test="username"]').type('locked_out_user')
-        cy.get('[data-test="password"]').type('secret_sauce')
-        cy.get('[data-test="login-button"]').click()
+        cy.login_lockedOutUser()
         cy.get('[data-test="error"]').should('be.visible').contains('Epic sadface: Sorry, this user has been locked out.')
     })
 
     it('Logar com um usuário problem_user', () => {
-        cy.get('[data-test="username"]').type('problem_user')
-        cy.get('[data-test="password"]').type('secret_sauce')
-        cy.get('[data-test="login-button"]').click()
+        cy.login_problemUser()
         cy.get('[data-test="title"]').should('be.visible')
     })
 
     it('Adicionar um produto ao carrinho', () => {
-        cy.get('[data-test="username"]').type('standard_user')
-        cy.get('[data-test="password"]').type('secret_sauce')
-        cy.get('[data-test="login-button"]').click()
-        cy.get('[data-test="title"]').should('be.visible')
+        cy.login_standardUser()
         cy.get('[data-test="add-to-cart-sauce-labs-backpack"]').click()
         cy.get('.shopping_cart_badge').should('be.visible')
     })
 
     it('Remover um produto do carrinho', () => {
-        cy.get('[data-test="username"]').type('standard_user')
-        cy.get('[data-test="password"]').type('secret_sauce')
-        cy.get('[data-test="login-button"]').click()
-        cy.get('[data-test="title"]').should('be.visible')
+        cy.login_standardUser()
         cy.get('[data-test="add-to-cart-sauce-labs-backpack"]').click()
         cy.get('[data-test="remove-sauce-labs-backpack"]').click()
         cy.get('[data-test="add-to-cart-sauce-labs-backpack"]').should('be.visible')
     })
 
     it('Comprar um produto do carrinho', () => {
-        cy.get('[data-test="username"]').type('standard_user')
-        cy.get('[data-test="password"]').type('secret_sauce')
-        cy.get('[data-test="login-button"]').click()
-        cy.get('[data-test="title"]').should('be.visible')
+        cy.login_standardUser()
         cy.get('[data-test="add-to-cart-sauce-labs-backpack"]').click()
         cy.get('[data-test="shopping-cart-link"]').click()
         cy.get('[data-test="checkout"]').click()
@@ -67,10 +51,7 @@ describe('My First Test', () => {
     })
 
     it('Comprar mais de um produto do carrinho', () => {
-        cy.get('[data-test="username"]').type('standard_user')
-        cy.get('[data-test="password"]').type('secret_sauce')
-        cy.get('[data-test="login-button"]').click()
-        cy.get('[data-test="title"]').should('be.visible')
+        cy.login_standardUser()
         cy.get('[data-test="add-to-cart-sauce-labs-backpack"]').click()
         cy.get('[data-test="add-to-cart-sauce-labs-bike-light"]').click()
         cy.get('[data-test="shopping-cart-link"]').click()
@@ -85,46 +66,31 @@ describe('My First Test', () => {
     })
 
     it('Selecionar a ordem dos produtos por Nome de A - Z', () => {
-        cy.get('[data-test="username"]').type('standard_user')
-        cy.get('[data-test="password"]').type('secret_sauce')
-        cy.get('[data-test="login-button"]').click()
-        cy.get('[data-test="title"]').should('be.visible')
+        cy.login_standardUser()
         cy.get('.product_sort_container').select('az')
         cy.get('.active_option').contains('Name (A to Z)')
     })
 
     it('Selecionar a ordem dos produtos por Nome de Z - A', () => {
-        cy.get('[data-test="username"]').type('standard_user')
-        cy.get('[data-test="password"]').type('secret_sauce')
-        cy.get('[data-test="login-button"]').click()
-        cy.get('[data-test="title"]').should('be.visible')
+        cy.login_standardUser()
         cy.get('.product_sort_container').select('za')
         cy.get('.active_option').contains('Name (Z to A)')
     })
 
     it('Selecionar a ordem dos produtos por Preço de baixo a alto', () => {
-        cy.get('[data-test="username"]').type('standard_user')
-        cy.get('[data-test="password"]').type('secret_sauce')
-        cy.get('[data-test="login-button"]').click()
-        cy.get('[data-test="title"]').should('be.visible')
+        cy.login_standardUser()
         cy.get('.product_sort_container').select('lohi')
         cy.get('.active_option').contains('Price (low to high)')
     })
 
     it('Selecionar a ordem dos produtos por Preço de alto a baixo', () => {
-        cy.get('[data-test="username"]').type('standard_user')
-        cy.get('[data-test="password"]').type('secret_sauce')
-        cy.get('[data-test="login-button"]').click()
-        cy.get('[data-test="title"]').should('be.visible')
+        cy.login_standardUser()
         cy.get('.product_sort_container').select('hilo')
         cy.get('.active_option').contains('Price (high to low)')
     })
 
     it('Carrinho com o produto removido deverá ter o total de itens como $0', () => {
-        cy.get('[data-test="username"]').type('standard_user')
-        cy.get('[data-test="password"]').type('secret_sauce')
-        cy.get('[data-test="login-button"]').click()
-        cy.get('[data-test="title"]').should('be.visible')
+        cy.login_standardUser();
         cy.get('[data-test="add-to-cart-sauce-labs-backpack"]').click()
         cy.get('[data-test="shopping-cart-link"]').click()
         cy.get('button[class="btn btn_secondary btn_small cart_button"]').click()
@@ -137,10 +103,7 @@ describe('My First Test', () => {
     })
 
     it('Realizar o Logout', () => {
-        cy.get('[data-test="username"]').type('standard_user')
-        cy.get('[data-test="password"]').type('secret_sauce')
-        cy.get('[data-test="login-button"]').click()
-        cy.get('[data-test="title"]').should('be.visible')
+        cy.login_standardUser()
         cy.get('#react-burger-menu-btn').click()
         cy.get('[data-test="logout-sidebar-link"]').click()
         cy.get('.login_logo').contains('Swag Labs').should('be.visible')
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,25 +1,34 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+Cypress.Commands.add('login_standardUser', () => {
+    cy.fixture('example').then((users) => {
+      const user = users.standardUser;
+      cy.get('[data-test="username"]').type(user.username)
+      cy.get('[data-test="password"]').type(user.password)
+      cy.get('[data-test="login-button"]').click()
+      cy.get('[data-test="title"]').should('be.visible')
+    })
+  })
+  
+Cypress.Commands.add('login_lockedOutUser', () => {
+    cy.fixture('example').then((users) => {
+      const user = users.lockedOutUser;
+      cy.get('[data-test="username"]').type(user.username)
+      cy.get('[data-test="password"]').type(user.password)
+      cy.get('[data-test="login-button"]').click()
+    })
+})
+
+Cypress.Commands.add('login_problemUser', () => {
+    cy.fixture('example').then((users) => {
+      const user = users.problemUser;
+      cy.get('[data-test="username"]').type(user.username)
+      cy.get('[data-test="password"]').type(user.password)
+      cy.get('[data-test="login-button"]').click()
+    })
+})
+
+Cypress.Commands.add('page', () =>{
+    cy.fixture('example').then((page) =>{
+        const main = page.url
+        cy.visit(main.swag_labs)
+    })
+})
